38c9c69ab9a000ad386057aaf82dc845fae2411f
Detective-Coder <aaronfrankenfield@gmail.com>
2021-03-14 09:44:23 -0400

    first commit

---
+++ src/index.js
@@ -0,0 +1,10 @@
+import React from "react"
+import ReactDOM from "react-dom"
+import {BrowserRouter as Router} from "react-router-dom"
+import "./index.css"
+
+ReactDOM.render(
+  <Router>
+
+  </Router>
+  , document.getElementById("root"))
